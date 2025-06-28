1d539dc35ec01eb8ab2c25a15bb5b03aa6c16f24
billchetch <45006134+billchetch@users.noreply.github.com>
2025-06-28 12:51:13 +0800

    asdf

--- SerialPortTest/Program.cs
+++ SerialPortTest/Program.cs
@@ -1,57 +1,57 @@
 ﻿using System.IO.Ports;
-using Chetch.Messaging;
+using Chetch.Utilities;
+
 
 namespace SerialPortTest;
 
 class Program
 {
+    class BTSerialConnection : SerialPortConnection
+    {
+
+        public BTSerialConnection(int baudRate, Parity parity, int dataBits, StopBits stopBits) : base(baudRate, parity, dataBits, stopBits) { }
+
+        protected override string GetPortName()
+        {
+            //return "COM1"; //windows
+            //return "/dev/tty.Bluetooth-Incoming-Port";
+            return "/dev/cu.Bluetooth-Incoming-Port";
+        }
+    }
+
     static void Main(string[] args)
     {
 
-        var portNames = SerialPort.GetPortNames();
-        
-        int baudRate = 9600;
-        Parity parity = Parity.None;
-        int dataBits = 8;
-        StopBits stopBits = StopBits.One;
-
-        String portName = "/dev/cu.usbmodem1401";
-        
+        //String portName = "/dev/cu.usbmodem1401"; //apple
+        //String portName = "/dev/ttyACM0"; //raspberry pi
         try
         {
-            using(var serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits)){
-                if(serialPort.IsOpen){
-                    Console.WriteLine("Serial port is open so closing...");
-                    serialPort.Close();
-                }
-                serialPort.DataReceived += (sender, eargs) => {
-                        Console.WriteLine("Data received");
-                        int dataLength = serialPort.BytesToRead;
-                        byte[] data = new byte[dataLength];
-                        int nbrDataRead = serialPort.Read(data, 0, dataLength);
-                        if (nbrDataRead == 0)
-                            return;
-
-                        string str = System.Text.Encoding.ASCII.GetString(data);
-                        String[] sentences = str.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-                        foreach (String sentence in sentences)
-                        {
-                            Console.WriteLine("Sentence {0}: ", sentence);
-                        }
-
-                    };
-                serialPort.Open();
-                Console.WriteLine("Opened {0}", portName);
-                ConsoleHelper.PK("Press a key to close");
-                serialPort.Close();
-                
-                Console.WriteLine("Closed {0}", portName);
+            BTSerialConnection btsp = new BTSerialConnection(115200, System.IO.Ports.Parity.None, 8, StopBits.One);
+            btsp.Connected += (sender, connected) =>
+            {
+                Console.WriteLine("Serial port connected {0}", connected);
+            };
+            btsp.DataReceived += (sender, data) =>
+            {
+                Console.WriteLine("Data {0} bytes received", data.Length);
+            };
+            ConsoleHelper.PK("Press a key to connect");
+            Console.WriteLine("Connecting bt serial connection");
+            btsp.Connect();
+            ConsoleHelper.PK("Press a key to send");
+            byte[] data = new byte[10];
+            data[0] = 1;
+            data[1] = 2;
+            for (int i = 0; i < 5; i++)
+            {
+                btsp.SendData(data);
+                ConsoleHelper.PK("Press to send again");
             }
-        } 
+        }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
         }
-
+        ConsoleHelper.PK("Press a key to end");
     }
 }
